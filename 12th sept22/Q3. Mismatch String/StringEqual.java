66c060a002db45cd899e845ab69433279d05b050
AtifRaja-Ansari <111556392+AtifRaja-Ansari@users.noreply.github.com>
2022-09-13 00:47:31 +0530

    Add files via upload

---
+++ 12th sept22/Q3. Mismatch String/StringEqual.java
@@ -0,0 +1,27 @@
+/*Write a program to throw a user-defined exception "String Mismatch Exception", if
+two strings are not equal (ignore the case)..*/
+package class_work;
+
+import java.util.Scanner;
+
+public class StringEqual {
+	
+		public static void main(String args[]) throws StringMismatchException {
+			Scanner sc= new Scanner(System.in);
+	String str="welcomehome";
+	System.out.println(str);
+	System.out.print("Enter the  String same as shown above:");
+	String a=sc.next();
+		try {
+			if (str.equalsIgnoreCase(a)) {
+				System.out.println("The entered string is  equal to the given string");
+			}else {
+				throw new  StringMismatchException("The entered string doesn't match the given string");
+			}
+			
+		} catch ( StringMismatchException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
